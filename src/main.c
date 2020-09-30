3df5dd0363742afe005ef943339c68c4a13bb8e6
Richmond Moore <richmondwmoore01@gmail.com>
2020-09-30 11:43:29 -0400

    Finished logic for reading ROM files and loading into memory array.

--- src/main.c
+++ src/main.c
@@ -16,11 +16,26 @@ typedef struct {
 int main() {
 	create_stack(16);
 
-	load_rom();
+	Chip8 chip8;
+
+	load_rom("../ROMS/BRIX", chip8.mem);
 	return 0;
 }
 
 void load_rom(char* path_to_file, BYTE* mem) {
 	FILE *fp = NULL;
 
+	if ((fp = fopen(path_to_file, "r")) == NULL) {
+		printf("ERROR: Cannot find file, unable to open!");
+		return;
+	}
+	fp = fopen(path_to_file, "r");
+
+	// Determines the size of the file
+	fseek(fp, 0L, SEEK_END);
+	unsigned int file_size = ftell(fp);
+	rewind(fp);
+
+	// Reads the file into memory starting at address 0x200
+	fread(&mem[0x200], 1, file_size, fp);
 }
