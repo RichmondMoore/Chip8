46ab1ab27498cc67dfc4d8df39ec532a96a0395c
Richmond Moore <richmondwmoore01@gmail.com>
2020-10-13 17:16:08 -0400

    Init

---
+++ include/Opcodes.h
@@ -0,0 +1,12 @@
+//
+// Created by rjwmo on 10/13/2020.
+//
+
+#ifndef CHIP8_OPCODES_H
+#define CHIP8_OPCODES_H
+
+typedef unsigned char BYTE;
+
+void execute_opcode(BYTE opcode);
+
+#endif //CHIP8_OPCODES_H
